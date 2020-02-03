3cb80ec92f9b349452a202e60a62818c0710f53f
eduardo-takeo <dudu.takeo96@gmail.com>
2020-02-03 12:26:27 -0300

    add: organizing component files

---
+++ src/containers/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Searchbox from '../components/Searchbox';
+import Scroll from '../components/Scroll';
+import CardList from '../components/CardList';
+import {robots} from '../robots';
+import './App.css'
+
+const state= {
+    robots: robots,
+    searchfield: ''
+}
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})        
+    }
+    
+    render() {
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
+        if(this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <Searchbox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
+    }
+}
+
+export default App;
